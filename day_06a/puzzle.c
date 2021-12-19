ad9846a61ab56f3dc8593552c28e6a554b47ff89
jay-j <jay-j@github.com>
2021-12-19 01:33:23 -0600

    Day 06 complete!

---
+++ day_06a/puzzle.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+#define FISH_TIMER_MAX 10
+#define GENERATIONS 80
+
+int fish_sum(int* population){
+   int sum=0;
+   for(size_t i=0; i<FISH_TIMER_MAX; ++i){
+      sum += population[i];
+   }
+   return sum;
+}
+
+void fish_timer_decrease(int* population){
+   for(size_t i=1; i<FISH_TIMER_MAX; ++i){
+      population[i-1] = population[i];
+   }
+}
+
+void fish_reproduce(int* population){
+   population[FISH_TIMER_MAX-1] = population[0]; // newborn fish
+   population[7] += population[0]; // reset reproduction timer
+   population[0] = 0;
+}
+
+int main(int argc, char *argv[]){
+
+   char filename[64];
+   sscanf(argv[1], "%s", filename);
+
+   // read in the file
+   FILE* fd;
+   fd = fopen(filename, "r");
+   assert(fd != NULL);
+   int sscanf_status;
+
+   // read the first line, figure out how many characters to use
+   char line_raw[1024];
+   char* fgets_status;
+   fgets_status = fgets(line_raw, sizeof(line_raw), fd);
+   assert(fgets_status != NULL);
+
+   // extract initial population from that first line
+   int population[FISH_TIMER_MAX];
+   memset(population, 0, FISH_TIMER_MAX*sizeof(*population));
+
+   char* line_working = line_raw;
+   char* token = strtok_r(line_working, ",", &line_working);
+   while (token != NULL){
+      // store the value
+      int num;
+      sscanf_status = sscanf(token, "%d", &num);
+      assert(sscanf_status == 1);
+      assert(num < FISH_TIMER_MAX);
+
+      population[num+1] += 1;
+      
+      token = strtok_r(line_working, ",", &line_working);
+   }
+   fclose(fd);
+
+   // now simulate!
+   // for generations...
+   for (size_t generation=0; generation<GENERATIONS; ++generation){
+
+      // decrease all timers
+      fish_timer_decrease(population);
+
+      // if zero turn to 6 AND add a new 8
+      fish_reproduce(population);
+   }
+
+   printf("%d\n", fish_sum(population));
+
+   return 0;
+}
